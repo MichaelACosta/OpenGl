53c3fba18642c23a77e4946ea33c7e4596a86f29
Costa <macosta@inf.ufpel.edu.br>
2018-03-03 23:49:58 -0300

    shaders: added 3 light color in shader

--- CG_UFPel/sources/main.cpp
+++ CG_UFPel/sources/main.cpp
@@ -267,13 +267,13 @@ int main(void)
         glm::vec3 lightPos3 = glm::vec3(4, 4, 4);
         glUniform3f(LightID3, lightPos3.x, lightPos3.y, lightPos3.z);
         
-        glm::vec3 lightColor1 = glm::vec3(1, 1, 1);
+        glm::vec3 lightColor1 = glm::vec3(1, 1, 0);
         glUniform3f(LightColorID1, lightColor1.x, lightColor1.y, lightColor1.z);
         
-        glm::vec3 lightColor2 = glm::vec3(1, 1, 1);
+        glm::vec3 lightColor2 = glm::vec3(1, 0, 0);
         glUniform3f(LightColorID2, lightColor2.x, lightColor2.y, lightColor2.z);
         
-        glm::vec3 lightColor3 = glm::vec3(1, 1, 1);
+        glm::vec3 lightColor3 = glm::vec3(0, 1, 0);
         glUniform3f(LightColorID3, lightColor3.x, lightColor3.y, lightColor3.z);
         
         
